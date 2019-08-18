9ced113240d9682144bf010de6610175ed0cbdb7
Jac08H <jac08h@protonmail.com>
2019-08-18 21:09:24 +0200

    hosting on heroku

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8443))
 
     bot = DrinkMixerBot(token)
-    bot.start_local()
+    bot.start_webhook('https://drink-mixer-bot.herokuapp.com/', port)
